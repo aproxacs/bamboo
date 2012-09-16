69d362ab20054dbb2c78a41c58d92b566d94f187
chang <aproxacs@gmail.com>
2012-09-16 13:22:35 +0900

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Secret::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  Pusher.app_id = '27762'
+  Pusher.key = 'ece90a7f4794c276c97e'
+  Pusher.secret = 'ad2ed9b102e3e1bacce6'
 end
